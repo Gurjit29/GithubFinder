652113c2280627d6f44a759b0d2108bbad4d7d92
Gurjit Khatkar <gurjitkhatkar@Gurjits-MacBook-Pro.local>
2021-06-22 18:27:12 -0700

    Implemented alert & validation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
@@ -10,7 +11,8 @@ class App extends Component {
 
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   //Load 30 profiles by default
@@ -38,6 +40,15 @@ class App extends Component {
     this.setState({users:[],loading: false});
   }
 
+  //Set Alert
+  setAlert = (msg,type) => {
+
+    this.setState({alert: {msg,type}})
+
+    setTimeout(() => this.setState({alert: null}), 5000);
+
+  }
+
 
   render(){
 
@@ -47,11 +58,14 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers}  clearUsers={this.clearUsers}
-          
-          showClear ={users.length > 0 ? true : false}
+          <Alert alert={this.state.alert} />
+          <Search 
+          searchUsers={this.searchUsers}  
+          clearUsers={this.clearUsers}
+          showClear ={users.length > 0 ? true : false }
+          setAlert = {this.setAlert}
           />
-        <Users loading={loading} users={this.state.users}/>
+        <Users loading={loading} users={users}/>
         </div>
     
       </div>
